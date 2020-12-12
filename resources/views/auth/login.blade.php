52c8aa354ebcd4ca507f2d67eb61eb93240c0131
bernaljuan <bernaljuankrlos@gmail.com>
2020-12-12 16:06:30 -0500

    estilos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,14 @@
-<link rel="stylesheet" href="css/login.css">
-<body class="body-login">
-    <div class="container-bar">
-        <input type="checkbox" id="btn-social">
-            <div class="icon-social">
-                <a href="{{ url('/') }}" class="icon-pinterest fab fa-pinterest">
-                    <h3>volver</h3>
-                    <span class="title">Pagina principal</span>
-                </a>
-            </div>
-        </div>
-    <form method="POST" action="{{ route('login') }}">
-        <div class="form-login form-login2">
-            <center><a href="{{ route('register') }}" class="l"><h3>Registrarse</h3></a></center><br><br>
-            <h1 class="titulo-login">Iniciar Sesión</h1>
-                @csrf
-               <div class="grupo-login">
-                    <input id="email" type="text" class="input-login form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    <label for="" class="label-login">Correo</label>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ 'Los datos no coinciden' }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="grupo-login">
-                    <input id="password" type="password" class=" input-login form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                    <label for="" class="label-login">Contraseña</label>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ "El correo debe tener algun @"}}</strong>
-                        </span>
-                   @enderror
-                </div>
-                <div class="form-check">                   
-                    <center>
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label class="form-check-label" for="remember">
-                                {{ __('Recordarme') }}
-                        </label>
-                    </center>
-                </div>
-                <button type="submit" class="boton-login">
-                    {{ __('Iniciar Sesión') }}
-                </button>
-
-                @if (Route::has('password.request'))
-                <center>    
-                    <a class="btn l btn-link" href="{{ route('password.request') }}">
-                        {{ __('Olvido su contraseña ?') }}
-                    </a>
-                </center>
-                @endif
-                           
-            </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="log.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Iniciar Sesión</title>
+</head>
+<body>
+    
 </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,84 +4,88 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/estilo2.css">
+        <link rel="stylesheet" href="css/all.min.css">
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+            integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <!-- Styles -->
+        <link href="{{ asset('css/submit.css') }}" rel="stylesheet">    
         <title>Pizza Shop</title>
     </head>
     <body>  
-        <header>
-            <div class="textos">
-                <a href="{{route ('personal') }}" class="btn"><h3>Personal</h3></a>
-                <a href="{{route ('familiar') }}" class="btn"><h3>Personal</h3></a>
-                <h1 class="titulo">Pizza Shop</h1>
-                <h3 class="subtitulo">El mejor sitio web de pizzas</h3>
-                <a  href="{{ route('login') }}" class="boton">Ingresar</a>
-            </div>
-            <div class="sesgoabajo"></div>
-        </header> 
+        <nav class="navbar navbar-expand-lg navbar-dark bg-danger menu">
+            <a class="navbar-brand" href="#"><h4>Pizza Shop</h4></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{route('login')}}">Ingrersar<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Registrarse</a>
+                    </li>
+                </ul>
+            </div>          
+        </nav>
         <main>
-            <section class="acercade">
-                <div class="contenedor">
-                    <h2 class="sobre-nosotros">Sobre Nosotros</h2>
-                    <h3 class="eslogan">Compra la mejor pizza.</h3>
-                    <p class="parrafo">Compra la pizza que quieras al tamaño que quieras y el sabor que quieras aqui. Contarás con los mejores servicios de calida, no solo en las pizzas sino tambien en tu entrega, sabiendo a tiempo real en que proceso se encuentra tu entrega.</p>
-                    <center>
-                    <a href="{{ route('register') }}" class="boton">Registrate</a></center>
-                </div>
-            </section>
-            <section class="galeria">
-                <div class="imagenes">
-                    <img src="im/4.jpg" alt="">
-                </div>
-                <div class="imagenes">
-                    <img src="im/4.jpg" alt="">
-                </div>
-                <div class="imagenes">
-                    <img src="im/4.jpg" alt="">
-                    <div class="encima">
-                        <h2>Pizza Shop</h2>
-                        <div></div>
+            <section class="">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="im/p.jpg" class="d-block w-100" alt="...">
+                        </div>
+
+                      <div class="carousel-item">
+                        <img src="im/4.jpg" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="im/p.jpg" class="d-block w-100" alt="...">
+                      </div>
                     </div>
-                </div>
-                <div class="imagenes">
-                    <img src="im/4.jpg" alt="">
-                </div>
-                <div class="imagenes">
-                    <img src="im/4.jpg" alt="">
-                </div>
-                <div class="sesgoabajo"></div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
             </section>
-            <section class="mienbros">
-                <div class="contenedor">
-                    <h2 class="sobre-nosotros">Nuestro Servicio</h2>
-                    <h3 class="eslogan">Conoce nuestros servicios</h3>
-                    <div class="cards">
-                        <div class="card">
-                            <img src="im/p.jpg" alt="">
-                            <h4>Proteccion de informacion</h4>
-                            <p>Siempre al servicio de nuestros clientes.</p>
-                        </div>
-                        <div class="card">
-                            <img src="im/p.jpg" alt="">
-                            <h4>Gestion de roles</h4>
-                            <p>Siempre al servicio de nuestros clientes.</p>
-                        </div>
-                        <div class="card">
-                            <img src="im/p.jpg" alt="">
-                            <h4>Almacenamiento segun tu servicio</h4>
-                            <p>Siempre al servicio de nuestros clientes.</p>
-                        </div>
+            <section class="container">
+                <center><h1 class="titulo">¿Que te ofecemos?<h1></center><hr>
+                <div class="row">
+                    <img src="im/pro.jpg" class="col-6" alt="">
+                    <div class="col-6">
+                        <h1 class="titulo2">Productos</h1>
+                        <p class="parrafo">Encuentra los mejores productos, con la mejor calidad al mejor precio y con garrantia de una alta calidad en los ingredientes.</p>
                     </div>
+                    <div class="col-6 mt-2">
+                        <h1 class="titulo2">Pedidos</h1>
+                        <p class="parrafo">Poidras pedir todos los productos que deses con la seguridad del envio y tendras una vitacora para ver estado de tu orden.</p>
+                    </div>
+                    <img src="im/cajas.jpg" class="col-6 mt-3 mb-4">
                 </div>
-                <div class="sesgoabajo-unico"></div>
-            </section>
-        </main>
-        <footer>
-            <div class="contenedor">
-                <h2 class="titulo-footer">Contactanos</h2>
-                <h3 class="subtitulo-footer">&copy;Pizza Shop</h3>
-                <h3 class="subtitulo-footer">pizzasho12@gmail.com</h3>
-                <h3 class="subtitulo-footer">3022839680</h3>
+            </section>   
+        <footer class="mt-5 row bg-danger p-2">
+            <div class="col-12 bg-danger text-white">
+                <h2 class="subtitulo-footer">Pizza Shop</h2>
+                <h4 class="ml-2">pizzasho12@gmail.com</h4>
+                <h4 class="ml-2">3022839680</h4>
+                <p>&copy; Pizza Shop todos los derecchos reserbados</p>
             </div>
         </footer>
     </body>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/submit.js') }}" defer></script>
 </html>
