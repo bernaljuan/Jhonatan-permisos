76e8d878c495be6e22a1aa6a5b3fcb857a80787f
camiloanva <camiloandrespaezvargas01@gmail.com>
2020-12-13 18:08:46 -0500

    pr

--- resources/views/products/products_add.blade.php
+++ resources/views/products/products_add.blade.php
@@ -2,39 +2,28 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <link rel="stylesheet" href="../../css/estilo2.css">
+    <link rel="stylesheet" href="../../css/log.css">
     <div class="container">
-        <div class="col-12">
-            <h1>Agregar existencia</h1>
-            <form method="POST" action="{{route("products.updateadd", [$product])}}">
-                @method("PUT")
-                @csrf
-
-                <div class="form-group">
-                    <label class="label">Producto:</label>
-                    <input type=»text» readonly=»readonly»  required value="{{$product->nombre}}" autocomplete="off" name="nombre"
-                           class="form-control"
-                           type="decimal(9,2)" placeholder="nombre">
-                </div>
-                
-                <div class="form-group">
-                    <label class="label">Existencia</label>
-                    <input  type=»text» readonly=»readonly» required value="{{$product->existencia}}" autocomplete="off" name="existencia"
-                           class="form-control"
-                           type="decimal(9,2)" placeholder="Existencia">
-                </div>
-
-                <div class="form-group">
-                    <label class="label">agregar unidades</label>
-                    <input required autocomplete="off" name="unidades_add" class="form-control"
-                           type="decimal(9,0)" placeholder="Unidades a agregar">
-                </div>
-
-                @include("notificacion")
-                <button class="btn btn-success">Guardar</button>
-                <a class="btn btn-primary" href="{{route("products.index")}}">Volver</a>
-            </form>
+        <div class="titulo r mt-5 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-xs-12 p-3 mx-auto">
+            <center>
+                <h1>Agregar Existencia</h1>
+            </center>
         </div>
+        <form method="POST" action="{{route("products.updateadd", [$product])}}" class="mx-auto col-xl-5 col-lg-5 col-md-5 col-sm-12 col-xs-12 formulario">
+            @method("PUT")
+            @csrf
+            <input type=»text» readonly=»readonly»  required value="{{$product->nombre}}" autocomplete="off" name="nombre"
+            class="col-12 inp mt-3 mb-3"
+            type="decimal(9,2)" placeholder="nombre">
+            <input  type=»text» readonly=»readonly» required value="{{$product->existencia}}" autocomplete="off" name="existencia"
+            class="col-12 inp mb-3"
+            type="decimal(9,2)" placeholder="Existencia">
+            <input required autocomplete="off" name="unidades_add" class="col-12 inp mb-3"
+            type="decimal(9,0)" placeholder="Unidades a agregar">
+            <button class="br col-12 p-2 mt-2 mb-2">Guardar</button>
+                <a class="btn btn-primary col-12 mb-3" href="{{route("products.index")}}">Volver</a>
+        </form>
     </div>
 @endsection
 
--- resources/views/products/products_edit.blade.php
+++ resources/views/products/products_edit.blade.php
@@ -5,9 +5,29 @@
     <link rel="stylesheet" href="../../css/estilo2.css">
     <link rel="stylesheet" href="../../css/log.css">
     <div class="container">
-        <div class="titulo mt-5 p-3 col-5 mx-auto r">
+        <div class="titulo mt-5 p-3 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-xs-12 mx-auto r">
             <center><H1>Editar Producto</H1></center>
         </div>
-        
+        <form method="POST" action="{{route("products.update", [$product])}}" class="p-3 col-xl-5 col-lg-5 col-md-5 col-sm-12 col-xs-12 mx-auto formulario">
+            @method("PUT")
+            @csrf
+            <input required value="{{$product->nombre}}" autocomplete="off" name="nombre"
+            class="col-12 inp mt-3 mb-3"
+            type="text" placeholder="Id">
+            <input required value="{{$product->descripcion}}" autocomplete="off" name="descripcion"
+            class="col-12 inp mb-3"
+            type="text" placeholder="Nombre">
+            <input required value="{{$product->precio_compra}}" autocomplete="off" name="precio_compra"
+            class="col-12 inp mb-3"
+            type="decimal(9,2)" placeholder="Precio de compra">
+            <input required value="{{$product->precio_venta}}" autocomplete="off" name="precio_venta"
+            class="col-12 inp mb-3"
+            type="decimal(9,2)" placeholder="Precio de venta">
+            <input required value="{{$product->existencia}}" autocomplete="off" name="existencia"
+            class="col-12 inp mb-3"
+            type="decimal(9,2)" placeholder="Existencia">
+            <button class="br col-12 p-2 mb-2">Guardar</button>
+            <a class="btn btn-primary col-12 p-2 mb-3" href="{{route("products.index")}}">Volver</a>
+        </form>
     </div>
 @endsection
