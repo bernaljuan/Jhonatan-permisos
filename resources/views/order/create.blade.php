da6d8d4599bc9d7dbb0b657490ff10ed709c03db
camiloanva <camiloandrespaezvargas01@gmail.com>
2020-12-13 14:04:24 -0500

    ordenar

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -1,110 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="../css/estilo2.css">
+<link rel="stylesheet" href="../css/log.css">
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-md-8">
-			<div class="card">
-				<div class="card-header">Ordenar Pizza</div>
-
-				<div class="card-body">
-					@if(session('status'))
-					<div class="alert alert-success">
-						{{session('status')}}
-					</div>
-					@endif
-
-					<div class="row">
-						<div class="col-lg-12">
-							<form method="POST" action=" {{route('user.order.store')}} " class="form-horizontal">
-								{{csrf_field()}}
-							<div class="col-sm-10">
-								<label class="col-sm-8 control-label"><strong>Pizza:</strong></label>
-								
-								<img src="{{ asset('storage').'/'. $dato->foto }}"  class="img-thumbnail img-fluid" alt="" width="150">
-							</div>
-								<br>
-								<div class="form-group">
-									<input type="hidden" name="product_id" value="{{ $dato->id }}">
-									<div class="col-sm-10">
-										<input type="text"  name="product_id" class="form-control"
-										value={{ $dato->nombre }} disabled>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-sm-8 control-label"><strong>Direccion</strong></label>
-									<div class="col-sm-10">
-										<input type="text" name="address" placeholder="Tu direccion" class="form-control" autofocus>
-									</div>
-								</div>		
-								<div class="form-group">
-									<label class="col-sm-8 control-label"><strong>Tamaño</strong></label>
-									<div class="col-sm-10">
-										<div>
-											<label><input type="radio" checked="" name="size" value="medium" id="medium"> Medio </label>
-										</div>
-										<div>
-											<label><input type="radio" name="size" value="large" id="large"> Grande </label>
-										</div>
-										<div>
-											<label><input type="radio" name="size" value="extra-large" id="extra-large"> Extra grande </label>
-										</div>
-									</div>
-								</div>
-
-								<div class="hr-line-dashed"></div>
-
-								<div class="form-group">
-									<label class="col-sm-8 control-label"><strong>Adiciones</strong></label>
-									<div class="col-sm-10">
-										<label class="checkbox-inline">
-											<input type="checkbox" name="toppings[]" value="pepperoni" id="pepperoni"> Pepperoni
-										</label>
-										<label class="checkbox-inline">
-											<input type="checkbox" name="toppings[]" value="extra-cheese" id="extra-cheese"> Extra queso
-										</label>
-										<label class="checkbox-inline">
-											<input type="checkbox" name="toppings[]" value="mushrooms" id="mushrooms"> Champiñones
-										</label>
-										<label class="checkbox-inline">
-											<input type="checkbox" name="toppings[]" value="ground-beef" id="ground-beef"> Carne molida
-										</label>
-										<label class="checkbox-inline">
-											<input type="checkbox" name="toppings[]" value="pineapple" id="pineapple"> Piña
-										</label>
-									</div>
-								</div>
-
-								<div class="hr-line-dashed"></div>
-								<div class="hr-line-dashed"></div>
-
-								<div class="form-group">
-									<label class="col-sm-8 control-label"><strong>Instrucciones</strong></label>
-									<div class="col-sm-10">
-										<input type="text" name="instructions" placeholder="Instrucciones especiales" class="form-control">
-									</div>
-								</div>
-
-								<div class="form-group">
-									<label class="col-sm-8 control-label"><strong>Cantidad</strong></label>
-									<div class="col-sm-10">
-										<input type="text" name="cantidad" placeholder="Cantidad" class="form-control" autofocus>
-									</div>
-								</div>	
-
-								<div class="hr-line-dashed"></div>
-
-								<div class="form-group">
-									<div class="col-sm-4 col-sm-offset-2">
-										<button class="btn btn-success" type="submit">Ordenar ahora</button>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="r p-3 col-xl-5 col-lg-5 col-md-6 col-sm-12 col-xs-12 titulo mx-auto mt-5">
+		<h1>Ordenar Pizza</h1>
 	</div>
+	<form method="POST" action="{{route('user.order.store')}}" class="col-xl-5 col-lg-5 col-md-6 col-sm-12 col-xs-12 formulario mx-auto">
+		{{csrf_field()}}
+		@if(session('status'))
+			<div class="alert alert-danger col-12">
+				{{session('status')}}
+			</div>
+		@endif
+		<img src="{{ asset('storage').'/'. $dato->foto }}"  class="img-thumbnail img col-12 img-fluid" alt="" width="150">
+		<input type="hidden" name="product_id" value="{{ $dato->id }}">
+		<input type="text"  name="product_id" class="col-12 inp mt-3 mb-3" value={{ $dato->nombre }} disabled>
+		<input type="text" name="address" placeholder="Direccion:" class="col-12 inp mb-3" autofocus>
+		<label>Tamaño:</label><hr>
+		<label><input type="radio" checked="" name="size" value="medium" id="medium"> Medio </label>
+		<label><input type="radio" name="size" value="large" id="large"> Grande </label>
+		<label><input type="radio" name="size" value="extra-large" id="extra-large"> Extra grande </label><hr>
+		<label>Adicionales:</label><hr>
+		<label class="checkbox-inline">
+			<input type="checkbox" name="toppings[]" value="pepperoni" id="pepperoni"> Pepperoni
+		</label>
+		<label class="checkbox-inline">
+			<input type="checkbox" name="toppings[]" value="extra-cheese" id="extra-cheese"> Extra queso
+		</label>
+		<label class="checkbox-inline">
+			<input type="checkbox" name="toppings[]" value="mushrooms" id="mushrooms"> Champiñones
+		</label>
+		<label class="checkbox-inline">
+			<input type="checkbox" name="toppings[]" value="ground-beef" id="ground-beef"> Carne molida
+		</label>
+		<label class="checkbox-inline">
+			<input type="checkbox" name="toppings[]" value="pineapple" id="pineapple"> Piña
+		</label>
+		<hr>
+		<input type="text" name="instructions" placeholder="Instrucciones especiales" class="col-12 inp mb-3">
+		<input type="text" name="cantidad" placeholder="Cantidad" class="col-12 inp mb-3" autofocus>
+		<button class="br col-12 p-3 mt-3 mb-3" type="submit">Ordenar Ahora</button>
+	</form>
 </div>
 @endsection
