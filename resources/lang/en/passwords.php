3ce5adb0b149c279b25a02be2c7f5e6eae46db03
bernaljuan <bernaljuankrlos@gmail.com>
2020-12-14 19:02:22 -0500

    correo

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,8 +14,8 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
+    'sent' => 'Hemos enviado un correo de recuperacion',
+    'throttled' => 'Espera un momento',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
 
